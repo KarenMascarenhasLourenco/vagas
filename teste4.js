55408da4d7eab052ab157c88cbe62f59eadbbed3
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-06-14 21:48:54 -0300

    Update teste4.js

--- teste4.js
+++ teste4.js
@@ -1,13 +1,16 @@
-var data =  require("./fakeData");
+var data = require("./fakeData");
 
-module.exports =  function(req, res) {
-  
-    var id =  req.query.id;
+module.exports = function (req, res) {
+ var { id } = req.query;
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+ const user = data.find((user) => user.id == id);
 
-    res.send(reg);
+ if (user) {
+  user.name = req.body.name;
+  user.job = req.body.job;
 
+  res.send(user);
+ } else {
+  res.status(404).send("Usuário não encontrado");
+ }
 };
